880618e30cb68206f207770f53eb6abe8dd43561
luisguareschi <luisguareschi@hotmail.com>
2022-04-21 12:41:47 +0200

    Agregadas instrucciones para seleccionar el cliente

--- Packages/gui/select_client_window/select_client_window.py
+++ Packages/gui/select_client_window/select_client_window.py
@@ -14,8 +14,18 @@ class SelectClientWindow:
         self.window_frame.tkraise()
         self.menu_bar = MenuBar(self.window_frame, gui)
         self.menu_bar.frame.place(relwidth=1)
+        # Titulo
+        title = tk.Label(self.window_frame, text='Selecciona el cliente:', font=("Arial", 24))
+        title.place(rely=.05, relx=.01)
+        # Descripcion
+        description = tk.Label(self.window_frame, text='El pedido que se esta subiendo es una orden de entrega nueva, '
+                                                       'por lo cual es necesario indicar el nombre del cliente.\n'
+                                                       'Si el cliente no existe ve a ajustes y agregalo primero. ',
+                               font=("Arial", 12), justify='left')
+        description.place(rely=.13, relx=.01)
+        # Tabla de clientes
         self.select_client_table = SelectClientTable(self.window_frame)
-        self.select_client_table.frame.place(rely=0.05, relx=0)
+        self.select_client_table.frame.place(rely=0.2, relx=0.01, relheight=.6)
 
     def show(self):
         self.window_frame.tkraise()
