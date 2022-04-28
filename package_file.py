58a831eeff6ba16e8914c8fb06a1c1020becbdd7
luisguareschi <luisguareschi@hotmail.com>
2022-04-28 14:08:56 +0200

    Ahora la aplicación se empaqueta utilizando un script especializado para ello

---
+++ package_file.py
@@ -0,0 +1,49 @@
+import PyInstaller.__main__
+import os
+import shutil
+from Packages.constants import download_latest_ver_folder
+
+
+def package_file(version: str):
+    """Funcion para empaquetar de manera mas sencilla la aplicacion"""
+    # Modificar la version del archivo en constants
+    constants_read = open(r'Packages/constants.py', 'r')
+    data = constants_read.readlines()
+    new_version = "actual_version = '{}'\n".format(version)
+    for n, line in enumerate(data):
+        print(n, line)
+        if 'actual_version' in line:
+            data[n] = new_version
+
+    constants_write = open(r'Packages/constants.py', 'w')
+    constants_write.writelines(data)
+    constants_write.close()
+    constants_read.close()
+
+    # Empaquetar la aplicacion usando pyinstaller
+    PyInstaller.__main__.run(['--onefile',
+                              '-F',
+                              'main.spec'])
+
+    # Cambiar el nombre
+    dist_folder = r'C:\Users\IRDGFRM\OneDrive-Deere&Co\OneDrive - Deere & Co\Documents\Python Projects\Bot Creacion de Pedidos\dist'
+    file_path = os.path.join(dist_folder, 'AutomatizacionSAP latest_release.exe')
+    new_path = os.path.join(dist_folder, 'AutomatizacionSAP {}.exe'.format(version))
+    os.rename(file_path, new_path)
+
+    # Mover el archivo existente a la versiones anteriores
+    old_path, file_name = None, None
+    for file_name in os.listdir(download_latest_ver_folder):
+        if 'AutomatizacionSAP' in file_name:
+            old_path = os.path.join(download_latest_ver_folder, file_name)
+            break
+    previous_versions_folder = os.path.join(download_latest_ver_folder, 'PreviousVersions')
+    shutil.move(old_path, os.path.join(previous_versions_folder, file_name))
+
+    # Copiar a la carpeta en linea de la app
+    file_path = new_path
+    source_path = os.path.join(download_latest_ver_folder, 'AutomatizacionSAP {}.exe'.format(version))
+    shutil.copy(file_path, source_path)
+
+
+package_file(version='v4.4.5')
