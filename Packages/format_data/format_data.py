2586887faf914b6b84f3bf45a01d23448f48f4cc
luisguareschi <luisguareschi@hotmail.com>
2022-05-13 11:10:52 +0200

    Simplificado el sistema para formatear la tabla escrita por el usuario
    En la AI se va a hacer este paso asi que ya no es necesario que tenga tantos filtros

--- Packages/format_data/format_data.py
+++ Packages/format_data/format_data.py
@@ -1,8 +1,6 @@
-from Packages.process_invoice.get_formats import get_formats
 import pandas as pd
 import numpy as np
 from .format_date import format_date
-from .format_quantity import format_quantity
 
 
 class FormatData:
@@ -11,64 +9,23 @@ class FormatData:
 
     def __init__(self, orders: pd.DataFrame):
         self.orders = orders
-        self.quantity_formats, self.ship_out_date_formats, self.arrival_date_formats = get_formats()
         self.client = self.orders['client'][self.orders.index[0]]
 
     def format_all(self):
-        # if validate_formats(self.client):
-        self.fix_blank_reference()
-        self.fix_blank_dates()
-        self.format_quantity()
-        self.ship_out_date()
-        self.arrival_date()
+        self.format_dates()
         self.fix_lower_case_reference()
-        # else:
-        #     print('Error en el formato de los datos')
         return self.orders
 
-    def fix_blank_reference(self):
-        previous_ref = self.orders['reference'][0]
+    def format_dates(self):
+        """Acepta los formatos de dd/mm/yyyy, dd/mm/yy\n
+        dd.mm.yyyy, dd.mm.yy y los convierte en dd/mm/yyyy"""
         for index in self.orders.index:
-            if str(self.orders['reference'][index]) == str(np.nan):
-                self.orders['reference'][index] = previous_ref
-            else:
-                previous_ref = self.orders['reference'][index]
-
-    def fix_blank_dates(self):
-        for index in self.orders.index:
-            if [str(self.orders['ship_out_date'][index]), str(self.orders['arrival_date'][index])] == [str(np.nan),
-                                                                                                       str(np.nan)]:
-                self.orders = self.orders.drop(index=index)
-
-    def format_quantity(self):
-        row = self.quantity_formats.loc[self.quantity_formats['client'] == self.client]
-        thousand_separators = {".": ",", ",": "."}  # Key=decimal_sep
-        for index in row.index:
-            decimal_sep = self.quantity_formats['decimal_separator'][index]
-            for ind in self.orders.index:
-                number = format_quantity(self.orders['quantity'][ind], decimal_sep)
-                self.orders['quantity'][ind] = number
-
-    def ship_out_date(self):
-        row = self.ship_out_date_formats.loc[self.ship_out_date_formats['client'] == self.client]
-        for index in row.index:
-            date_format = str(self.ship_out_date_formats['date_format'][index])
-            for ind in self.orders.index:
-                if str(self.orders['ship_out_date'][ind]) != str(np.NaN):
-                    ship_out_date = str(self.orders['ship_out_date'][ind])
-                    if ship_out_date != str(np.nan):
-                        date = format_date(ship_out_date, date_format)
-                        self.orders['ship_out_date'][ind] = date
-
-    def arrival_date(self):
-        row = self.arrival_date_formats.loc[self.arrival_date_formats['client'] == self.client]
-        for index in row.index:
-            date_format = str(self.arrival_date_formats['date_format'][index])
-            for ind in self.orders.index:
-                arrival_date = str(self.orders['arrival_date'][ind])
-                if arrival_date != str(np.nan):
-                    date = format_date(arrival_date, date_format)
-                    self.orders['arrival_date'][ind] = date
+            arrival_date = str(self.orders['arrival_date'][index])
+            ship_out_date = str(self.orders['ship_out_date'][index])
+            if arrival_date != str(np.nan):
+                self.orders['arrival_date'][index] = format_date(arrival_date)
+            if ship_out_date != str(np.nan):
+                self.orders['ship_out_date'][index] = format_date(ship_out_date)
 
     def fix_lower_case_reference(self):
         """Funcion que transforma las letras de la
--- Packages/format_data/format_date.py
+++ Packages/format_data/format_date.py
@@ -1,102 +1,31 @@
 import datetime
 
 
-def format_date(client_v: str, date_format: str) -> str:
-    """Transforma la fecha obtenida por la AI y la transforma al
-    formato universal de dd/mm/yyyy"""
-    week = ''
+def format_date(date: str) -> str:
+    """Acepta los formatos de dd/mm/yyyy, dd/mm/yy\n
+        dd.mm.yyyy, dd.mm.yy y los convierte en dd/mm/yyyy"""
     day = ''
     month = ''
     year = ''
-    if date_format == 'dd/mm/yyyy':
-        if 'W' in client_v:
-            position = 0
-            for char in client_v:
-                if char.isdigit():
-                    if len(week) < 2:
-                        week = week.__add__(char)
-                    else:
-                        year = year.__add__(char)
-                    if len(week) == 1:
-                        if client_v[position + 2].isdigit():
-                            week = '0' + week
-                position = position + 1
-        elif 'M' not in client_v:
-            position = 0
-            for char in client_v:
-                if char.isdigit():
-                    if len(day) < 2:
-                        day = day.__add__(char)
-                    elif len(month) < 2:
-                        month = month.__add__(char)
-                    else:
-                        year = year.__add__(char)
-                    if len(month) == 1:
-                        if client_v[position + 2].isdigit():
-                            month = '0' + month
-                    if len(day) == 1:
-                        if client_v[position + 2].isdigit():
-                            day = '0' + day
-                position = position + 1
 
-        elif 'M' in client_v:
-            day = '01'
-            position = 0
-            for char in client_v:
-                if char.isdigit():
-                    if len(month) < 2:
-                        month = month.__add__(char)
-                    else:
-                        year = year.__add__(char)
-                    if len(month) == 1:
-                        if client_v[position + 2].isdigit():
-                            month = '0' + month
-                    if len(day) == 1:
-                        if client_v[position + 2].isdigit():
-                            day = '0' + day
-                position = position + 1
+    position = 0
+    for char in date:
+        if char.isdigit():
+            if len(day) < 2:
+                day = day.__add__(char)
+            elif len(month) < 2:
+                month = month.__add__(char)
+            else:
+                year = year.__add__(char)
+            if len(month) == 1:
+                if date[position + 2].isdigit():
+                    month = '0' + month
+            if len(day) == 1:
+                if date[position + 2].isdigit():
+                    day = '0' + day
+        position = position + 1
 
-    elif date_format == 'mm/dd/yyyy':
-        if 'M' not in client_v:
-            position = 0
-            for char in client_v:
-                if char.isdigit():
-                    if len(month) < 2:
-                        month = month.__add__(char)
-                    elif len(day) < 2:
-                        day = day.__add__(char)
-                    else:
-                        year = year.__add__(char)
-                    if len(month) == 1:
-                        if client_v[position + 2].isdigit():
-                            month = '0' + month
-                    if len(day) == 1:
-                        if client_v[position + 2].isdigit():
-                            day = '0' + day
-                position = position + 1
-        elif 'M' in client_v:
-            day = '01'
-            position = 0
-            for char in client_v:
-                if char.isdigit():
-                    if len(month) < 2:
-                        month = month.__add__(char)
-                    else:
-                        year = year.__add__(char)
-                    if len(month) == 1:
-                        if client_v[position + 2].isdigit():
-                            month = '0' + month
-                    if len(day) == 1:
-                        if client_v[position + 2].isdigit():
-                            day = '0' + day
-                position = position + 1
-    if week != '':
-        date = '{}-W{}'.format(year, week)
-        day = '-4'  # Jueves
-        result = datetime.datetime.strptime(date + day, "%Y-W%W-%w")
-        predicted_v = result.strftime('%d/%m/%Y')
-    else:
-        if len(year) == 2:
-            year = '20'+year
-        predicted_v = '{}/{}/{}'.format(day, month, year)
-    return predicted_v
+    if len(year) == 2:
+        year = '20'+year
+    formatted_date = '{}/{}/{}'.format(day, month, year)
+    return formatted_date
