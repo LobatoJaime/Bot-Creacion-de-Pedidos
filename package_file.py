a99b1349f332763ec1191bcaa5bf021146cddaa2
JuanDelPinoJD <124151748+JuanDelPinoJD@users.noreply.github.com>
2023-03-13 08:26:06 +0100

    v6.1.3

--- package_file.py
+++ package_file.py
@@ -67,5 +67,5 @@ def package_file(version: str, dist_folder: str):
 if __name__ == '__main__':
     local_folder = r'C:\Users\VJZP49U\Documents\PythonProjects\Temporal-dist'  # Cambiar a disposicion
     online_folder = r'\\fcefactory1\PROGRAMAS_DE_PRODUCCION\6.Planificacion\Bot Creacion de Pedidos\dist'
-    package_file(version='v6.1.2',
+    package_file(version='v6.1.3',
                  dist_folder=online_folder)
